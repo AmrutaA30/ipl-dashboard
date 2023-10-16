6357bd26eecc44c38fbf88fbf96bb7a39b9e3a1a
amruta30 <amruta.ayachit30@gmail.com>
2023-10-16 15:58:16 -0400

    React app with Team page

--- src/main/java/io/javaproject/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javaproject/ipldashboard/controller/TeamController.java
@@ -1,5 +1,6 @@
 package io.javaproject.ipldashboard.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,9 @@ import io.javaproject.ipldashboard.model.Team;
 import io.javaproject.ipldashboard.repository.MatchRepository;
 import io.javaproject.ipldashboard.repository.TeamRepository;
 
+
 @RestController
+@CrossOrigin
 public class TeamController {
     
     private TeamRepository teamRepo;
